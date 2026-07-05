2f128e5b6ba2b20ee70b63f2a37ba4fc03596f2c
ThetaHalo <theta@eps.lol>
2026-07-05 17:52:01 -0500

    unfinished

---
+++ FlashlightUs/UI/ModUpdateMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using FlashlightUs.UI.Patches;
+using UnityEngine;
+using VentLib;
+using VentLib.Utilities.Optionals;
+using VentLib.Version.Updater;
+using VentLib.Version.Updater.Github;
+
+namespace FlashlightUs.UI;
+
+// based on: https://github.com/Lotus-AU/LotusContinued/blob/8a00694ae082339c9abad299945ce1f1522be74c/src/GUI/Menus/ModUpdateMenu.cs
+public class ModUpdateMenu: MonoBehaviour
+{
+    private static readonly StandardLogger log = LoggerFactory.GetLogger<StandardLogger>(typeof(ModUpdateMenu));
+
+    public void Open()
+    {
+        
+    }
+
+    public static void BeginUpdate(Release release)
+    {
+        UnityOptional<ModUpdateMenu>.Of(ModUpdaterPatches.ModUpdateMenu).Handle(o => o.Open(), () => ModUpdaterPatches.IsReady = true);
+        ModUpdateMenu.AddUpdateItem("Lotus", null, ex => ModUpdater.Update(errorCallback: ex)!);
+        Assembly ventAssembly = typeof(Vents).Assembly;
+
+        if (release.ContainsDLL($"{ventAssembly.GetName().Name!}.dll"))
+            ModUpdateMenu.AddUpdateItem("VentFrameworkContinued", null, ex => ModUpdater.Update(ventAssembly, ex)!);
+    }
+}
---
+++ FlashlightUs/UI/Patches/ModUpdaterPatches.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using HarmonyLib;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using VentLib.Utilities;
+using VentLib.Utilities.Optionals;
+using Object = UnityEngine.Object;
+
+namespace FlashlightUs.UI.Patches;
+
+[HarmonyPatch(typeof(MainMenuManager), nameof(MainMenuManager.Start))]
+public class ModUpdaterPatches
+{
+    public static ModUpdateMenu ModUpdateMenu;
+    internal static UnityOptional<GameObject> UpdateButton = UnityOptional<GameObject>.Null();
+
+    public static bool IsReady;
+    
+    // based on portions of https://github.com/Lotus-AU/LotusContinued/blob/8a00694ae082339c9abad299945ce1f1522be74c/src/GUI/Patches/SplashPatch.cs
+    public static void Prefix(MainMenuManager __instance)
+    {
+        ModUpdateMenu = __instance.gameObject.AddComponent<ModUpdateMenu>();
+        
+        var playButton = __instance.playButton;
+        
+        PassiveButton updateButton = Object.Instantiate(playButton, __instance.transform);
+        Async.Schedule(() =>
+        {
+            TextMeshPro tmp = updateButton.GetComponentInChildren<TextMeshPro>();
+            tmp.text = "Update Found!";
+            tmp.enableWordWrapping = true;
+        }, 0.1f);
+        updateButton.transform.localPosition += new Vector3(0f, 1.85f);
+        updateButton.transform.localScale -= new Vector3(0f, 0.25f);
+        //updateButton.GetComponentInChildren<ButtonRolloverHandler>().OutColor = ModConstants.Palette.GeneralColor5;
+        //updateButton.GetComponentInChildren<SpriteRenderer>().color = ModConstants.Palette.GeneralColor5;
+        Button.ButtonClickedEvent buttonClickedEvent = new();
+        updateButton.GetComponentInChildren<PassiveButton>().OnClick = buttonClickedEvent;
+        buttonClickedEvent.AddListener((UnityAction)(Action)(() => ModUpdateMenu.Open()));
+
+        UpdateButton = UnityOptional<GameObject>.Of(updateButton.gameObject);
+
+        if (!FlashlightUsPlugin.ModUpdater.HasUpdate) updateButton.gameObject.SetActive(false);
+    }
+}
