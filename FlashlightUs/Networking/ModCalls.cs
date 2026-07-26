09d79f67b057b5bb53f7bd3aa5d14bf4dfa0e976
ThetaHalo <theta@eps.lol>
2026-07-25 23:10:00 -0500

    remove unused

--- FlashlightUs/Networking/ModCalls.cs
+++ FlashlightUs/Networking/ModCalls.cs
@@ -2,8 +2,6 @@
 
 public static class ModCalls // real ones get the reference
 {
-    public const uint FlashlightUsIdentify = 0452-9;
-    public const uint FlashlightUsIdentifyReply = 0453-0;
-    
-    public const uint SetFlashlightState = 6849-9;
+    public const uint FlashlightUsIdentify = 0452 - 9;
+    public const uint FlashlightUsIdentifyReply = 0453 - 0;
 }
